c3ad5ddcec71981ee38e168222d69e342cda0086
Yasmin Guiniver <94757345+YasminGuiniver@users.noreply.github.com>
2023-02-28 08:41:16 -0300

    Add files via upload

---
+++ contato.blade.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/contato.css"/>
+    </head>
+
+    <body>
+        <div class="contato-tela">
+            <h1>Fale conosco</h1>
+            <div class="form"> 
+                <form method="POST" id="form">
+                    <label>Nome*</label>
+                    <input type="text" placeholder="Digite seu nome completo">
+
+                    <label>Email*</label>
+                    <input type="email" placeholder="Ex: seuemail@email.com">
+                    
+                    
+                    <label>Assunto*</label>
+                    <input type="text" placeholder="Digite o assunto">
+                    
+                    
+                    <label>Mensage,*</label>
+                    <input type="text" placeholder="Digite um mensagem">
+                    
+                    <button class="enviar" id="enviar">Enviar</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
